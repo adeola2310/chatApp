51a7b7f989223fc2ccbe6fd834288c32b6d3c71a
adeola2310 <adekoyejoadeola@gmail.com>
2021-10-22 15:48:11 +0100

    refactor: disabled style for button

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -5,12 +5,12 @@ import Header from "../../components/Header/Header";
 const LayoutContainer = (props)=>{
     const { children } = props;
     return(
-        <>
+        <div className="layout">
             <Header/>
             <main className="main">
                 {children}
             </main>
-        </>
+        </div>
     );
 };
 
